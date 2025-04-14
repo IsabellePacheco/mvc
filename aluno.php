ab8bfc434fa79ae9a5c3b47e5ff25231ab8fcc48
IsabellePacheco <isapachecosenai@gmail.com>
2025-04-14 16:33:32 -0300

    aluno

--- aluno.php
+++ aluno.php
@@ -1,10 +1,8 @@
 <?php
-// defina classe chamada aluno
+
 
 class Aluno {
-    // criar uma propriedade privada (encapsulamento) chamada $alunos
-    // essa propriedade é um array que armazena dados de alunos.
-    // cada aluno é representado por array associativo com nome e idade.
+
 
     private $alunos =[
         ['nome' => 'Maria', 'idade' => 19],
@@ -12,8 +10,6 @@ class Aluno {
         ['nome' => 'Melissa', 'idade' => 9]
     ];
 
-    // define um metodo publico chamado listarAlunos.
-    // esse metodo serve para listar "retornar" o conteudo do array $alunos
     public function listarAlunos(){
         return $this ->alunos;
     }
